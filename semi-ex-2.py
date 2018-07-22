7cef7d4f5347894f344968e01ca70abd8b8fd076
Ta Tan Phat <phat@codelynx.io>
2018-07-23 00:14:43 +0700

    fas

--- semi-ex-2.py
+++ semi-ex-2.py
@@ -6,14 +6,40 @@ from sklearn.model_selection import LeaveOneOut
 from sklearn import  tree
 
 from sklearn.model_selection import KFold
-cv1 = KFold(n_splits= 5)
+
+def testWithSffs(alg, train_set, train_target, test_set, test_target, attr_mask):
+	train_set = train_set[:, attr_mask]
+	test_set = test_set[:, attr_mask]
+	print "-----------------Test---------------"
+	print "Model:"
+	print alg
+	print "Number of feature: " + str(len(attr_mask))
+	print train_set.shape
+	print test_set.shape
+	print '------------'
+
+	alg.fit(train_set, train_target)
+	result = alg.score(test_set,test_target)
+	print "Result: " + str(result)
+
+
+
+cv1 = KFold(n_splits= 15)
 cv2 = LeaveOneOut()
 id3 = tree.DecisionTreeClassifier()
 lal_data = np.load('./pp-result/filtered/filteredLalMatrix0.5.npy')
 target = np.load('./ng-result/target.txt')
 selectedFeatures = np.load('./selectedFeatures.npy')
 
-cv_result = cross_val_score(svm_clf , lal_data, target, cv=cv1)
+
+
+train_set = lal_data[:, selectedFeatures]
+# test_set = target[:, selectedFeatures]
+cv_result = cross_val_score(svm_clf , lal_data, target, cv=cv2)
+print "avg accuracy: " +  str(cv_result.mean())
+print "detail : " + str(cv_result)
+
+cv_result = cross_val_score(svm_clf , train_set, target, cv=cv2)
 print "avg accuracy: " +  str(cv_result.mean())
 print "detail : " + str(cv_result)
 
--- semi-ex.py
+++ semi-ex.py
@@ -43,13 +43,13 @@ unl_data = np.load('./pp-result/filtered/filteredUnlMatrix0.5.npy')
 target = np.load('./ng-result/target.txt')
 attr = np.load('./pp-result/filtered/filteredAttr0.5.npy')
 
-random_slice = np.random.choice(a = lal_data.shape[0],  size  =  50* len(lal_data)/100, replace = False)
+# random_slice = np.random.choice(a = lal_data.shape[0],  size  =  50* len(lal_data)/100, replace = False)
 
-train_set = lal_data[random_slice,:]
-train_target = target[random_slice]
+# train_set = lal_data[random_slice,:]
+# train_target = target[random_slice]
 
-test_set = np.delete(lal_data, random_slice, axis = 0)
-test_target = np.delete(target, random_slice)
+# test_set = np.delete(lal_data, random_slice, axis = 0)
+# test_target = np.delete(target, random_slice)
 
 
 # print lal_data.shape
@@ -57,16 +57,16 @@ test_target = np.delete(target, random_slice)
 # print attr.shape
 # print target.shape
 
-id3.fit(train_set, train_target)
-beforeSemi = id3.score(test_set, test_target)
-print "Init accuracy: " +  str(beforeSemi)
+# id3.fit(lal_data, target)
+# beforeSemi = id3.score(test_set, test_target)
+# print "Init accuracy: " +  str(beforeSemi)
 # print "detail : " + str(cv_result)
 
-acc_arr = []
-num_es  = []
+# acc_arr = []
+# num_es  = []
 sffs_model = sffs()
-acc_arr.append(beforeSemi)
-num_es.append(0)
+# acc_arr.append(beforeSemi)
+# num_es.append(0)
 
 
 
@@ -75,24 +75,24 @@ num_es.append(0)
 
 bagging = BaggingClassifier(base_estimator = None, n_estimators = 19, bootstrap = True)
 fw = Framework(number_of_iter = 10 , algorithm = bagging, confident =  0.5,  verbose =  "DETAIL")
-fw.fit(train_set,unl_data,train_target)
-id3.fit(fw.extended_lal_data, fw.extended_data_target)
-afterSemi = id3.score(test_set, test_target)
-print "After semi accuracy: " +  str(afterSemi)
+fw.fit(lal_data,unl_data,target)
+# id3.fit(fw.extended_lal_data, fw.extended_data_target)
+# afterSemi = id3.score(test_set, test_target)
+# print "After semi accuracy: " +  str(afterSemi)
 
-sffs_model.loaddatafromarray(fw.extended_lal_data, fw.extended_data_target , attr, test_set, test_target)
+sffs_model.loaddatafromarray(fw.extended_lal_data, fw.extended_data_target , attr, lal_data, target)
 accuracy, selected_feature =  sffs_model.sffs_predefine(25)
 
 np.save('./selectedFeatures', selected_feature)
 
-print 'After sffs: ' + str(accuracy)
+# print 'After sffs: ' + str(accuracy)
 
-testWithSffs(id3,fw.extended_lal_data, fw.extended_data_target,test_set,test_target,selected_feature)
-testWithSffs(svm_clf,fw.extended_lal_data, fw.extended_data_target,test_set,test_target,selected_feature)
+# testWithSffs(id3,fw.extended_lal_data, fw.extended_data_target,test_set,test_target,selected_feature)
+# testWithSffs(svm_clf,fw.extended_lal_data, fw.extended_data_target,test_set,test_target,selected_feature)
 
-testWithSffs(sgd_clf,fw.extended_lal_data, fw.extended_data_target,test_set,test_target,selected_feature)
+# testWithSffs(sgd_clf,fw.extended_lal_data, fw.extended_data_target,test_set,test_target,selected_feature)
 
-testWithSffs(gnb_clf,fw.extended_lal_data, fw.extended_data_target,test_set,test_target,selected_feature)
+# testWithSffs(gnb_clf,fw.extended_lal_data, fw.extended_data_target,test_set,test_target,selected_feature)
 
 
 # acc_arr.append(afterSemi)
